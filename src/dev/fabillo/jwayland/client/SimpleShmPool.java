ae3a308812fd16faa7ad242478b3bcd892e451dd
Fabillotic <fabillo552@gmail.com>
2023-02-02 20:28:10 +0000

    Fix SimpleShmPool indentation

--- src/dev/fabillo/jwayland/client/SimpleShmPool.java
+++ src/dev/fabillo/jwayland/client/SimpleShmPool.java
@@ -3,51 +3,50 @@ package dev.fabillo.jwayland.client;
 import java.nio.ByteBuffer;
 
 public final class SimpleShmPool {
+	
+	private int fd = -1;
+	private ByteBuffer data = null;
 
-    private int fd = -1;
-    private ByteBuffer data = null;
+	private SimpleShmPool(int fd, ByteBuffer data) {
+		this.fd = fd;
+		this.data = data;
+	}
 
-    private SimpleShmPool(int fd, ByteBuffer data) {
-        this.fd = fd;
-        this.data = data;
-    }
+	public static SimpleShmPool create(int size) {
+		int fd = allocate_shm_file(size);
+		ByteBuffer data = map_shm(fd, size);
+		SimpleShmPool pool = new SimpleShmPool(fd, data);
+		return pool;
+	}
 
-    public static SimpleShmPool create(int size) {
-        int fd = allocate_shm_file(size);
-        ByteBuffer data = map_shm(fd, size);
-        SimpleShmPool pool = new SimpleShmPool(fd, data);
-        return pool;
-    }
+	public void close_fd() {
+		if(fd != -1) {
+			close_shm_fd(fd);
+		}
+	}
 
-    public void close_fd() {
-        if(fd != -1) {
-            close_shm_fd(fd);
-        }
-    }
+	public void destroy() {
+		close_fd();
+		if(data != null) {
+			unmap_shm(data);
+			data = null;
+		}
+	}
 
-    public void destroy() {
-        close_fd();
-        if(data != null) {
-            unmap_shm(data);
-            data = null;
-	    }
-    }
+	public ByteBuffer asByteBuffer() {
+		return data;
+	}
 
-    public ByteBuffer asByteBuffer() {
-        return data;
-    }
+	public int getFileDescriptor() {
+		return fd;
+	}
 
-    public int getFileDescriptor() {
-        return fd;
-    }
+	private static native int allocate_shm_file(int size);
+	private static native ByteBuffer map_shm(int fd, int size);
+	private static native void unmap_shm(ByteBuffer buffer);
+	private static native void close_shm_fd(int fd);
 
-    private static native int allocate_shm_file(int size);
-    private static native ByteBuffer map_shm(int fd, int size);
-    private static native void unmap_shm(ByteBuffer buffer);
-    private static native void close_shm_fd(int fd);
-
-    static {
-	System.loadLibrary("jwayland");
-    }
+	static {
+		System.loadLibrary("jwayland");
+	}
 }
-
